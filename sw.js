25a1fba27d4caffd33048e555ea23884f1ea9636
alisheikhow <alisheikhow1@gmail.com>
2025-09-27 23:11:43 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'aed-calc-v1';
+const CACHE = 'aradesh-aed-calc-v1';
 const ASSETS = [
   './',
   './index.html',
@@ -20,14 +20,11 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  if (ASSETS.includes(url.pathname.replace(/^[^#?]*\/([^#?]*)$/, './$1')) || url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      }).catch(() => caches.match('./index.html')))
-    );
-  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
