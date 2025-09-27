c47b4b321045967c0e4c232eb0de5f58e5daccf2
alisheikhow <alisheikhow1@gmail.com>
2025-09-27 22:18:00 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'aed-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (ASSETS.includes(url.pathname.replace(/^[^#?]*\/([^#?]*)$/, './$1')) || url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      }).catch(() => caches.match('./index.html')))
+    );
+  }
+});
